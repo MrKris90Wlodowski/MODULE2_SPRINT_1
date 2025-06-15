f359f86a6e891dd3b134e574ef4f773f0d86269b
Kris1990_PL <MRK1990@wp.pl>
2025-06-15 16:49:04 +0200

    add functional check status API

--- API/script.js
+++ API/script.js
@@ -1,5 +1,3 @@
-// const { createElement } = require("react");
-
 const searchInput = document.getElementById("searchInput");
 const aliveStatus = document.getElementById("aliveStatus");
 const deadStatus = document.getElementById("deadStatus");
@@ -8,15 +6,18 @@ const usersContainer = document.getElementById("usersContainer");
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
 const errorMessage = document.getElementById("errorMessage");
+const formContainer = document.getElementById("formContainer");
 
 const API = "https://rickandmortyapi.com/api/character";
 
 let currentPage = 1;
 let maxPage;
+let statusUser = "alive";
 
 async function loadUsers() {
-  try {
-    const response = await fetch(`${API}/?page=${currentPage}`);
+    usersContainer.innerHTML="";
+    try {
+    const response = await fetch(`${API}/?page=${currentPage}&status=${statusUser}`);
     if (!response.ok) {
       throw new Error("FAILED TO FETCH USERS");
     }
@@ -29,13 +30,41 @@ async function loadUsers() {
       const userImage = document.createElement("div");
       userImage.classList.add("imageContainer");
       userImage.style.backgroundImage = `url(${user.image})`;
+      const userName = document.createElement("p");
+      userName.classList.add("userName", "displayFlex");
+      userName.textContent=`${user.name}`;
+      const userStatus = document.createElement("p");
+      userStatus.textContent=`Gender: ${user.gender}`;
+      userStatus.classList.add("userStatus");
+      userStatus.textContent=`Status: ${user.status}`
+      const userGender = document.createElement("p");
+      userGender.classList.add("userGender");
+      userGender.textContent=`Gender: ${user.gender}`;
       newUser.appendChild(userImage);
+      newUser.appendChild(userName);
+      newUser.appendChild(userStatus);
+      newUser.appendChild(userGender);
       usersContainer.appendChild(newUser);
     });
   } catch (error) {
     errorMessage.textContent = `FAILED TO LOAD USER :( ${error}`;
   }
 }
+
+formContainer.addEventListener("change", (event) => {
+    const status = event.target.dataset.status;
+    if (status === "alive") {
+            statusUser = "alive";
+        } else if (status === "dead") {
+            statusUser = "dead";
+        } else if (status === "unknown") {
+           statusUser = "unknown"; 
+        } else {
+            return
+        }
+    currentPage = 1;
+    loadUsers()
+});
 
 prevButton.addEventListener("click", () => {
   if (currentPage === 1) {
